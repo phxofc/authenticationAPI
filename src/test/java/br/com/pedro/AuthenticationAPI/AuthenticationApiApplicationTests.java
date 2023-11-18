9542d5d62dbd678bab2462cc31cb16c5579c3312
phxofc <phdfrocha99@gmail.com>
2023-11-18 16:13:58 -0300

    First Commit Project

---
+++ src/test/java/br/com/pedro/AuthenticationAPI/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.AuthenticationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
